cdb8605835cbb284bafc5a387829a72b7c1d524a
Glebova <e.tulzakova@mail.ru>
2023-02-26 18:44:42 +0400

    Initial commit

---
+++ src/test/java/ru/glebova/mockito/MockitoApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.glebova.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
